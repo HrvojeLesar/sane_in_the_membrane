42cc5263f4b8e19ab7485e424c73c843bc9d4554
Hrvoje Lesar <hrvoje.lesar1@hotmail.com>
2026-01-26 13:53:20 +0100

    Utils

---
+++ utils/src/SynchronizedAccess.hpp
@@ -0,0 +1,73 @@
+#ifndef SANE_IN_THE_MEMBRANE_UTILS_SYNCHRONIZED_ACCESS
+#define SANE_IN_THE_MEMBRANE_UTILS_SYNCHRONIZED_ACCESS
+
+#include <mutex>
+#include <shared_mutex>
+#include <type_traits>
+
+namespace sane_in_the_membrane::utils {
+
+    template <typename T, typename LockType = std::unique_lock<std::shared_mutex>>
+    class AccessGuard {
+      public:
+        AccessGuard(T& object, std::shared_mutex& mutex) : m_object(object), m_lock(mutex) {}
+        AccessGuard(AccessGuard<T, LockType>&& other) : m_object(other.m_object), m_lock(std::move(other.m_lock)) {}
+
+        AccessGuard<T, LockType>& operator=(AccessGuard<T, LockType>&& other) {
+            this->m_object = other.m_object;
+            this->m_lock   = std::move(other.m_lock);
+
+            return *this;
+        }
+
+        T* operator->() const {
+            return &this->m_object;
+        }
+
+        T& operator*() const {
+            return *this->m_object;
+        }
+
+      private:
+        T&       m_object;
+        LockType m_lock;
+    };
+
+    template <typename T>
+    using SharedAccessGuard = AccessGuard<const T, std::shared_lock<std::shared_mutex>>;
+
+    template <typename T>
+    class UniqueAccess {
+      public:
+        UniqueAccess() : m_object() {}
+        UniqueAccess(const T& object) : m_object(object) {}
+        UniqueAccess(const T&& object) : m_object(std::move(object)) {}
+
+        UniqueAccess<T>& operator=(const T& object) {
+            m_object = object;
+
+            return *this;
+        }
+
+        UniqueAccess<T>& operator=(T&& object) {
+            m_object = std::move(object);
+
+            return *this;
+        }
+
+        AccessGuard<T> access() {
+            return AccessGuard<T>(m_object, m_mutex);
+        }
+
+        template <typename O = T, typename = std::enable_if_t<!std::is_const_v<O>>>
+        SharedAccessGuard<const T> shared_access() {
+            return SharedAccessGuard<const T>(m_object, m_mutex);
+        }
+
+      private:
+        T                 m_object;
+        std::shared_mutex m_mutex;
+    };
+}
+
+#endif // !SANE_IN_THE_MEMBRANE_UTILS_SYNCHRONIZED_ACCESS
