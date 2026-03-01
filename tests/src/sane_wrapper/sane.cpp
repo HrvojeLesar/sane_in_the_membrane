e04219baefa98b86bc19be8e1b6acfe5d5ac5c87
Hrvoje Lesar <hrvoje.lesar1@hotmail.com>
2026-03-01 20:38:29 +0100

    Sane open and read from device test

--- tests/src/sane_wrapper/sane.cpp
+++ tests/src/sane_wrapper/sane.cpp
@@ -2,12 +2,29 @@
 
 #include <Sane.hpp>
 #include <sane/sane.h>
+#include <SaneMock.hpp>
 
 using namespace sane_in_the_membrane::sane;
+using namespace sane_in_the_membrane::mock;
 
 TEST(Sane, SaneInit) {
     CSane       sane{};
     CSaneStatus status{SANE_STATUS_GOOD};
     EXPECT_EQ(SANE_STATUS_GOOD, sane.init());
     EXPECT_EQ(true, sane.is_ok());
+}
+
+TEST(Sane, OpenDevice) {
+    CSane sane{};
+    CSaneState::instance().generate_mock_device();
+
+    auto devices = sane.get_devices();
+    ASSERT_EQ(1, devices.size());
+
+    auto device = devices.front().lock();
+
+    EXPECT_EQ(SANE_STATUS_GOOD, device->open());
+    EXPECT_EQ(SANE_STATUS_GOOD, device->start());
+    EXPECT_EQ(SANE_STATUS_GOOD, device->read());
+    EXPECT_EQ(CMockHandle::MAX_READ_LENGTH, device->buffer().read_len);
 }
